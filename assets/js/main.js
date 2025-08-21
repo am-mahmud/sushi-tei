1d90549ae179b491d9c5518f8e58f9939d4a7a1e
am-mahmud <am.mahmudasif@gmail.com>
2025-08-21 14:00:33 -0400

    Function added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,40 @@
-document.getElementById('phone-number').addEventListener('keypress', validPhoneNumber);
+document.getElementById('btn-submit').addEventListener('click', validEmailAddress, validPhoneNumber);
 
+// Email validation 
 
-function validPhoneNumber(contact){
-    if(typeof contact !=- 'string'){
+let email = document.getElementById('email-address').value;
+
+function validEmailAddress(email){
+    if (typeof contact !== 'string') {
         return false;
     }
-    if(contact.length == 11 && number.startsWith('01') && contact.includes(" ") !== true){
+    if(email.includes('@')){
         return true;
     }else{
         return false;
     }
 }
+
+console.log('validEmailAddress');
+
+// Phone number validation
+
+let contact = document.getElementById('phone-number').value;
+
+
+function validPhoneNumber(contact) {
+    if (typeof contact !== 'string') {
+        return false;
+    }
+    if (
+        contact.length === 11 && 
+        contact.startsWith('01') && 
+        !contact.includes(" ")
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log('validPhoneNumber');
